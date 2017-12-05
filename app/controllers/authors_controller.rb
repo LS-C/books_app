c1c43a15df9b10016c890d7e92a23c4389bea8af
LS-C <suzy.cho111@gmail.com>
2017-12-05 11:19:06 -0500

    protected member/client profile/account routes

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,6 @@
 class AuthorsController < ApplicationController
     before_action :authenticate_author!
+    before_action :access_to_own_profile, only: [:show]
 
 
     def index
@@ -30,6 +31,14 @@ class AuthorsController < ApplicationController
     private
     def author_params
         params.require(:author).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+    end
+
+    def access_to_own_profile
+      @author = Author.find(params[:id])
+
+      if current_author != @author
+        redirect_to root_path, notice: "Sorry, but you are only allowed to view your own profile page."
+      end
     end
 
 
