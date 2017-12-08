ba4e08f4b1c676a45280342a47758c709f8a8b44
LS-C <suzy.cho111@gmail.com>
2017-12-08 17:10:59 -0500

    fixed edit form and added category to book/show

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,24 +1,24 @@
 
-$(function() {
+$('form#edit_book').submit(function(e) {
+  e.preventDefault()
+  const url = $(this).attr('action')
+  console.log(url)
+  const params = $(this).serialize()
 
-  $('form#edit_book').submit(function(e) {
-    e.preventDefault()
-    const url = $(this).attr('action')
-    console.log(url)
-    const params = $(this).serialize()
-
-    $.ajax({
-      method: "patch",
-      url: url,
-      data: params,
-    })
-    .done(function(data) {
-      const updatedBook = data
-      updatedBook.title ==="" ? $('.book-title').text('untitled') :   $('.book-title').text(updatedBook.title)
-      updatedBook.description ==="" ? $('.book-description').text('Not scripted yet') : $('.book-description').text("Synopsis: "+ updatedBook.description)
-      updatedBook.category === "" ? $('.book-title').text('need category') :   $('.book-title').text(updatedBook.title)
-      $('form#edit_book')[0].reset();
-    })
+  $.ajax({
+    method: "patch",
+    url: url,
+    data: params
   })
+  .done(function(data) {
+    console.log(data)
+    const updatedBook = data
+    updatedBook.title ==="" ? $('.book-title').text('untitled') :   $('.book-title').text(updatedBook.title)
+    updatedBook.description ==="" ? $('.book-description').text('Not scripted yet') : $('.book-description').text("Synopsis: "+ updatedBook.description)
+    updatedBook.category === "" ? $('.book-title').text('need category') :   $('.book-title').text(updatedBook.title)
+    // $('input#book_title').val(nextBook.title)
+    // $('textarea#book_description').text(nextBook.description)
+    // $('input#book_category').val(nextBook.category)
 
+  })
 })
